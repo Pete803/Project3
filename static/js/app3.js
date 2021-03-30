eefeceebc82493a9ea28e1b0df1087b924a6eb19
pedro aida <pete803@hotmail.com>
2021-03-30 17:28:57 -0400

    lowercase data

--- static/js/app3.js
+++ static/js/app3.js
@@ -21,7 +21,7 @@ var svg = d3.select("body").append("svg")
     "translate(" + margin.left + "," + margin.top + ")");
 
 // get the data
-d3.csv("../static/Data/pppData.csv").then(function (data) {
+d3.csv("../static/data/pppData.csv").then(function (data) {
 
   // format the data
   data.forEach(function (d) {
